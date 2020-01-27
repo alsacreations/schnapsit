9a5d667abed9a80e81a7b24f1177db477f30ad5b
Raphael <raphael@goetter.fr>
2020-01-27 10:15:37 +0100

    suppression Google Analytics

--- html.php
+++ html.php
@@ -53,23 +53,7 @@ ob_start(); ?>
     if( isset( $_POST['codehtml'] ) && !empty( $_POST['codehtml'] ) ) {
       echo "\n" . "    " . $_POST['codehtml'];
     }
-
-    if( isset( $userChoice['ga'] ) && $userChoice['ga'] == 'on' ) {
-
-      if( isset($userChoice['ua']) && empty( $userChoice['ua'] ) ) {
-        $userChoice['ua'] = 'UA-XXXXXXXX-X';
-      }
-
-      echo "\n" . "    " . "<script>
-      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-      ga('create', " . '\'' . $userChoice['ua'] . '\'' . ", 'XXXXXXXXXXX.TLD');
-      ga('send', 'pageview');
-    </script>";
-    } ?>
+    ?>
 
   </body>
 </html><?php
